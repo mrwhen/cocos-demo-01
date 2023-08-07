7753a796ce9f11a9640dff72b8b45f65023ce94c
ext.ligen7 <ext.ligen7@jd.com>
2023-08-07 19:41:29 +0800

    feat: init

---
+++ assets/Game.ts
@@ -0,0 +1,45 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = Game
+ * DateTime = Mon Aug 07 2023 18:50:39 GMT+0800 (中国标准时间)
+ * Author = mrwhen
+ * FileBasename = Game.ts
+ * FileBasenameNoExtension = Game
+ * URL = db://assets/Game.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+ 
+@ccclass('Game')
+export class Game extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+
+    start () {
+        // [3]
+        console.log('hello world');
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/zh/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/zh/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/zh/scripting/life-cycle-callbacks.html
+ */
